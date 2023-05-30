796a2bf0c76152d6763ec029abfec29d060cefe3
ngc.918 <nicolas.g9182@gmail.com>
2023-05-29 23:58:58 -0400

    further dashboard customization

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,14 +1,51 @@
+import { Box, IconButton, Stack, useTheme } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-const DashboardLayout = () => {
+import Logo from "../../assets/Images/logo.ico";
+import { Nav_Buttons } from "../../data";
 
-  return (
-    <>
-      Dashboard Layout
-      <Outlet />
-    </>
-  );
+const DashboardLayout = () => {
+	const theme = useTheme();
+
+	return (
+		<>
+			<Box
+				p={2}
+				sx={{
+					backgroundColor: theme.palette.background.paper,
+					boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+					height: "100vh",
+					width: 100,
+				}}
+			>
+				<Stack
+					direction="column"
+					alignItems={"center"}
+					sx={{ width: "100%" }}
+					spacing={3}
+				>
+					<Box
+						sx={{
+							backgroundColor: theme.palette.primary.main,
+							height: 64,
+							width: 64,
+							borderRadius: 2.5,
+						}}
+					>
+						<img src={Logo} alt="logo" />
+					</Box>
+					<Stack spacing={3}>
+						{Nav_Buttons.map((e) => (
+							<IconButton key={e.index}>{e.icon}</IconButton>
+						))}
+					</Stack>
+					<Divider></Divider>
+				</Stack>
+			</Box>
+			<Outlet />
+		</>
+	);
 };
 
 export default DashboardLayout;
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,31 +9,33 @@ import { DEFAULT_PATH } from "../config";
 import LoadingScreen from "../components/LoadingScreen";
 
 const Loadable = (Component) => (props) => {
-  return (
-    <Suspense fallback={<LoadingScreen />}>
-      <Component {...props} />
-    </Suspense>
-  );
+	return (
+		<Suspense fallback={<LoadingScreen />}>
+			<Component {...props} />
+		</Suspense>
+	);
 };
 
+// "/app"
+
 export default function Router() {
-  return useRoutes([
-    {
-      path: "/",
-      element: <DashboardLayout />,
-      children: [
-        { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
-        { path: "app", element: <GeneralApp /> },
-        
-        { path: "404", element: <Page404 /> },
-        { path: "*", element: <Navigate to="/404" replace /> },
-      ],
-    },
-    { path: "*", element: <Navigate to="/404" replace /> },
-  ]);
+	return useRoutes([
+		{
+			path: "/",
+			element: <DashboardLayout />,
+			children: [
+				{ element: <Navigate to={DEFAULT_PATH} replace />, index: true },
+				{ path: "app", element: <GeneralApp /> },
+
+				{ path: "404", element: <Page404 /> },
+				{ path: "*", element: <Navigate to="/404" replace /> },
+			],
+		},
+		{ path: "*", element: <Navigate to="/404" replace /> },
+	]);
 }
 
 const GeneralApp = Loadable(
-  lazy(() => import("../pages/dashboard/GeneralApp")),
+	lazy(() => import("../pages/dashboard/GeneralApp"))
 );
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
