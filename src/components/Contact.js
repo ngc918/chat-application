2cdf99f1e72671fe09ba43ea100d6f7f7a6bbcac
ngc.918 <nicolas.g9182@gmail.com>
2023-06-15 23:52:22 -0400

    some contact section done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,45 @@
-import { Box } from "@mui/material";
 import React from "react";
+import { Box, IconButton, Stack, Typography, useTheme } from "@mui/material";
+import { X } from "phosphor-react";
+import { useDispatch } from "react-redux";
+import { ToggleSideBar } from "../redux/slices/app";
 
 const Contact = () => {
-	return <Box sx={{ width: 320 }}>s</Box>;
+	const theme = useTheme();
+	const dispatch = useDispatch();
+	return (
+		<Box sx={{ width: 320, height: "100vh" }}>
+			<Stack sx={{ height: "100%" }}>
+				<Box
+					sx={{
+						boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+						width: "100%",
+						backgroundColor:
+							theme.palette.mode === "light"
+								? "#F8FAFF"
+								: theme.palette.background,
+					}}
+				>
+					<Stack
+						sx={{ height: "100%", p: 2 }}
+						direction="row"
+						alignItems={"center"}
+						justifyContent={"space-between"}
+						spacing={3}
+					>
+						<Typography variant="subtitle2">Contact Info</Typography>
+						<IconButton
+							onClick={() => {
+								dispatch(ToggleSideBar());
+							}}
+						>
+							<X />
+						</IconButton>
+					</Stack>
+				</Box>
+			</Stack>
+		</Box>
+	);
 };
 
 export default Contact;
--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -3,9 +3,11 @@ import Chats from "./Chats";
 import { Box, Stack, useTheme } from "@mui/material";
 import Conversation from "../../components/Conversation";
 import Contact from "../../components/Contact";
+import { useSelector } from "react-redux";
 
 const GeneralApp = () => {
 	const theme = useTheme();
+	const { sidebar } = useSelector((store) => store.app);
 
 	return (
 		<Stack direction={"row"} sx={{ width: "100%" }}>
@@ -16,7 +18,7 @@ const GeneralApp = () => {
 			<Box
 				sx={{
 					height: "100%",
-					width: "calc(100vw - 740px)",
+					width: sidebar.open ? "calc(100vw - 740px)" : "calc(100vw - 420px)",
 					backgroundColor:
 						theme.palette.mode === "light"
 							? "#F0F4FA"
@@ -27,7 +29,7 @@ const GeneralApp = () => {
 				<Conversation />
 			</Box>
 			{/*Contact*/}
-			<Contact />
+			{sidebar.open && <Contact />}
 		</Stack>
 	);
 };
