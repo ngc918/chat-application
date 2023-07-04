42acadac7cef803fdeb6bc53036484ac23fc5e73
ngc.918 <nicolas.g9182@gmail.com>
2023-07-03 22:29:04 -0400

    created group page and create group form

--- src/pages/dashboard/Chats.js
+++ src/pages/dashboard/Chats.js
@@ -1,133 +1,22 @@
-import { faker } from "@faker-js/faker";
 import {
 	Box,
 	IconButton,
 	Stack,
 	Typography,
-	InputBase,
-	styled,
-	alpha,
 	Button,
 	Divider,
-	Avatar,
-	Badge,
 	useTheme,
 } from "@mui/material";
-import {
-	Archive,
-	ArchiveBox,
-	CircleDashed,
-	MagnifyingGlass,
-} from "phosphor-react";
+import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
 import React from "react";
 import { ChatList } from "../../data";
 import { SimpleBarStyle } from "../../components/Scrollbar";
-
-const StyledBadge = styled(Badge)(({ theme }) => ({
-	"& .MuiBadge-badge": {
-		backgroundColor: "#44b700",
-		color: "#44b700",
-		boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-		"&::after": {
-			position: "absolute",
-			top: 0,
-			left: 0,
-			width: "100%",
-			height: "100%",
-			borderRadius: "50%",
-			animation: "ripple 1.2s infinite ease-in-out",
-			border: "1px solid currentColor",
-			content: '""',
-		},
-	},
-	"@keyframes ripple": {
-		"0%": {
-			transform: "scale(.8)",
-			opacity: 1,
-		},
-		"100%": {
-			transform: "scale(2.4)",
-			opacity: 0,
-		},
-	},
-}));
-
-const ChatElement = ({ id, name, img, msg, time, unread, online }) => {
-	const theme = useTheme();
-	return (
-		<Box
-			sx={{
-				width: "100%",
-				borderRadius: 1,
-				backgroundColor:
-					theme.palette.mode === "#fff"
-						? "#fff"
-						: theme.palette.background.default,
-			}}
-			p={2}
-		>
-			<Stack
-				className="scrollbar"
-				direction={"row"}
-				alignItems={"center"}
-				justifyContent="space-between"
-			>
-				<Stack direction="row" spacing={2}>
-					{online ? (
-						<StyledBadge
-							overlap="circular"
-							anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-							variant="dot"
-						>
-							<Avatar src={faker.image.avatar()} />
-						</StyledBadge>
-					) : (
-						<Avatar src={faker.image.avatar()} />
-					)}
-
-					<Stack spacing={0.3}>
-						<Typography variant="subtitle2">{name}</Typography>
-						<Typography variant="caption">{msg}</Typography>
-					</Stack>
-				</Stack>
-				<Stack spacing={2} alignItems={"center"}>
-					<Typography sx={{ fontWeight: 600 }} variant="caption">
-						{time}
-					</Typography>
-					<Badge color="primary" badgeContent={unread}></Badge>
-				</Stack>
-			</Stack>
-		</Box>
-	);
-};
-
-const Search = styled("div")(({ theme }) => ({
-	position: "relative",
-	borderRadius: 20,
-	backgroundColor: alpha(theme.palette.common.white, 1),
-	marginRight: theme.spacing(2),
-	marginLeft: 0,
-	width: "100%",
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-	padding: theme.spacing(0, 2),
-	height: "100%",
-	position: "absolute",
-	pointerEvents: "none",
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-	color: "inherit",
-	"& .MuiInputBase-input": {
-		paddingTop: theme.spacing(1),
-		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-		width: "100%",
-	},
-}));
+import {
+	Search,
+	SearchIconWrapper,
+	StyledInputBase,
+} from "../../components/Search";
+import ChatElement from "../../components/ChatElement";
 
 const Chats = () => {
 	const theme = useTheme();
--- src/routes/index.js
+++ src/routes/index.js
@@ -38,7 +38,8 @@ export default function Router() {
 				{ element: <Navigate to={DEFAULT_PATH} replace />, index: true },
 				{ path: "app", element: <GeneralApp /> },
 				{ path: "settings", element: <Settings /> },
-
+				{ path: "group", element: <GroupPage /> },
+				{ path: "call", element: <CallPage /> },
 				{ path: "404", element: <Page404 /> },
 				{ path: "*", element: <Navigate to="/404" replace /> },
 			],
@@ -50,19 +51,15 @@ export default function Router() {
 const GeneralApp = Loadable(
 	lazy(() => import("../pages/dashboard/GeneralApp"))
 );
-
 const LoginPage = Loadable(lazy(() => import("../pages/auth/Login")));
-
 const RegisterPage = Loadable(lazy(() => import("../pages/auth/Register")));
-
 const ResetPasswordPage = Loadable(
 	lazy(() => import("../pages/auth/ResetPassword"))
 );
-
 const NewPasswordPage = Loadable(
 	lazy(() => import("../pages/auth/NewPassword"))
 );
-
 const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
-
+const CallPage = Loadable(lazy(() => import("../pages/dashboard/Call")));
+const GroupPage = Loadable(lazy(() => import("../pages/dashboard/Group")));
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
