85fa7653053647e8b82cbccc0667f2716142b353
ngc.918 <nicolas.g9182@gmail.com>
2023-08-08 23:46:46 -0400

    frontend/backend integration (wip)

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -14,6 +14,7 @@ const rootPersistConfig = {
 
 const rootReducer = combineReducers({
 	app: appReducer,
+	auth: authReducer,
 });
 
 export { rootPersistConfig, rootReducer };
--- src/redux/slices/app.js
+++ src/redux/slices/app.js
@@ -1,8 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-//
-import { dispatch } from "../store";
-
 const initialState = {
 	sidebar: {
 		open: false,
@@ -29,13 +26,13 @@ export default slice.reducer;
 
 //
 export function ToggleSideBar() {
-	return async () => {
+	return async (dispatch, getState) => {
 		dispatch(slice.actions.toggleSidebar());
 	};
 }
 
 export function UpdateSidebarType(type) {
-	return async () => {
+	return async (dispatch, getState) => {
 		dispatch(
 			slice.actions.updateSidebarType({
 				type,
