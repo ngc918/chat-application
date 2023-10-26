fa47fef08ad435e8cbacb91a842e7f5681f12363
ngc.918 <nicolas.g9182@gmail.com>
2023-10-25 22:44:01 -0400

    frontend issues

--- src/sections/auth/LoginForm.js
+++ src/sections/auth/LoginForm.js
@@ -74,7 +74,7 @@ const LoginForm = () => {
 					type={showPassword ? "text" : "password"}
 					InputProps={{
 						endAdornment: (
-							<InputAdornment>
+							<InputAdornment position="end">
 								<IconButton
 									onClick={() => {
 										setShowPassword(!showPassword);
